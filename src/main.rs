f81e25fc1c20420ec2c45c33f89e7720cde478ec
Edith <c.e.ike@edu.salford.ac.uk>
2025-03-27 18:00:15 -0700

    Adds documentation

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,22 @@ use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 use std::sync::{mpsc, Arc, Mutex};
 
+/// A `Worker` is a type that handles a single thread and runs a job received
 struct Worker {
     id: usize,
     thread: thread::JoinHandle<Arc<Mutex<mpsc::Receiver<Job>>>>
 }
 
 impl Worker {
+    /// Creates a new `Worker`
+    /// 
+    /// Arguments:  
+    /// - **id**: a usize to uniquely identify the worker  
+    /// - **receiver**: a channel receiver wrapped in a Mutex wrapped in an Arc
+    /// 
+    /// This method creates a new thread and passes a closure containing an infinite loop of waiting
+    /// for the mutex to be free, acquiring the lock, getting the available job in the channel, freeing 
+    /// the lock and then executing the job.
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || loop {
             let job = receiver.lock().unwrap().recv().unwrap();
@@ -21,14 +31,27 @@ impl Worker {
     }
 }
 
+/// A `Job` is a type alias for any function that runs once and implements `Send` and `static`
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
+
+/// A `ThreadPool` is a struct that handles multiple threads using workers, and communicates with
+/// them by sending `Job`s through a channel, the first available worker picks up the job and executes it
 struct ThreadPool {
     workers: Vec<Worker>,
     sender: mpsc::Sender<Job>,
 }
 
 impl ThreadPool {
+    /// Creates a new `ThreadPool`
+    /// 
+    /// Arguments:
+    /// - **size**: the number of workers in the `ThreadPool`
+    /// 
+    /// This method creates a channel and holds onto the sender, passing the receiver to each new 
+    /// `Worker` created.
+    /// An Arc<Mutex> is used so that the channel can be passed between threads and so that only 
+    /// one worker has access to the mutex of the receiver at a time
     fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
         let mut workers = Vec::with_capacity(size);
@@ -43,12 +66,26 @@ impl ThreadPool {
         ThreadPool { workers, sender }
     }
     
+    /// Executes a job in a thread
+    /// 
+    /// Arguments:
+    /// - **f**: any object that implements `FnOnce()` + `Send` + `'static`
+    /// 
+    /// This method creates a new job and sends it to a channel from the sender, to be consumed by 
+    /// the first available receiver, which will be a thread in one of the workers
     fn execute<F>(&self, f: F) where F: FnOnce() + Send + 'static {
         let job = Box::new(f);
         self.sender.send(job).unwrap();
     }
 }
 
+/// Handles an HTTP connection 
+/// 
+/// Arguments:
+/// - **stream**: a mutable TcpStream that represents a single TCP connection or HTTP request
+/// 
+/// This method reads the stream using a BufReader and uses the request line to identify what path
+/// was called and how to handle each one.
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
